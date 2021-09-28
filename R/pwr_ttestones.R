22c13e8e98ff901f563ec3668ec151a1f49540aa
Jan Simson <jan.simson@three-2-one.com>
2021-09-28 17:00:29 +0200

    Add code from jPower module, add src notice & autoformat it

---
+++ R/pwr_ttestones.R
@@ -0,0 +1,23 @@
+# Originally based on https://github.com/richarddmorey/jpower
+
+ttestOneSClass <- R6::R6Class(
+    "ttestOneSClass",
+    inherit = ttestPSClass,
+    private = list(
+        type = "one.sample",
+        .init = function() {
+            super$.init()
+
+            self$results$setTitle("One Sample T-Test")
+        }
+    ),
+    public = list(
+        asSource = function() {
+            paste0(private$.package, "::", "ttestOneS", "(", private$.asArgs(), ")")
+        },
+        initialize = function(...) {
+            super$initialize(...)
+            private$.name <- "ttestOneS"
+        }
+    )
+)
