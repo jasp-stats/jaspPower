04c70cdaed713d2f33885a2de64067d0f5f1bf19
Jan Simson <jan.simson@three-2-one.com>
2021-10-13 20:53:42 +0200

    Use correct analysis classes in main analysis

--- R/Power.R
+++ R/Power.R
@@ -1,13 +1,16 @@
 Power <- function(jaspResults, dataset, options) {
   if (options$test == "ttest_independent") {
     instance <- ttestISClass$new(jaspResults, options)
-    instance$run()
   } else if (options$test == "ttest_paired") {
-
+    instance <- ttestPSClass$new(jaspResults, options)
   } else if (options$test == "ttest_onesample") {
-
+    instance <- ttestOneSClass$new(jaspResults, options)
   } else if (options$test == "anova") {
+    instance <- anovaClass$new(jaspResults, options)
+  }
 
+  if (!is.null(instance)) {
+    instance$run()
   }
 
   return()
