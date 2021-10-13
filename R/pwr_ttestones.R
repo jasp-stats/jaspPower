7f2ed19df74e1f5faca76545d6eee1e57556ad2b
Jan Simson <jan.simson@three-2-one.com>
2021-10-13 21:56:46 +0200

    Fix one sample t-test

--- R/pwr_ttestones.R
+++ R/pwr_ttestones.R
@@ -4,20 +4,7 @@ ttestOneSClass <- R6::R6Class(
     "ttestOneSClass",
     inherit = ttestPSClass,
     private = list(
-        type = "one.sample",
-        .init = function() {
-            super$.init()
-
-            self$results$setTitle("One Sample T-Test")
-        }
-    ),
-    public = list(
-        asSource = function() {
-            paste0(private$.package, "::", "ttestOneS", "(", private$.asArgs(), ")")
-        },
-        initialize = function(...) {
-            super$initialize(...)
-            private$.name <- "ttestOneS"
-        }
+        # TODO: Show test title somewhere
+        type = "one.sample"
     )
 )
