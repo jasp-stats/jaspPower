9b9610e8f7b66f7c93bd588ec1b30446cc5a83c7
Jan Simson <jan.simson@three-2-one.com>
2021-11-02 13:31:24 +0100

    Move plotSettings into utils

--- R/pwr_utils.R
+++ R/pwr_utils.R
@@ -124,3 +124,20 @@ striped.Arrows <- function(col1 = "black", col2 = "white", ...) {
   segments(..., col = col1, lty = 1)
   shape::Arrows(..., col = col2, lty = 2)
 }
+
+ttestPlotSettings <- list(
+  lens = 20,
+  x.axis.n = 8,
+  pow.n.levels = 10,
+  curve.n = 128,
+  maxn = 100,
+  max.scale = 1.5,
+  mind = 0,
+  maxd = 2,
+  background.alpha = .7,
+  # stripe.cols = pal(pow.n.levels)[c(1,pow.n.levels)]
+  stripe.cols = c("black", "black")
+)
+ttestPlotSettings$pal <- function(...) {
+  viridis::viridis(..., alpha = ttestPlotSettings$background.alpha)
+}
